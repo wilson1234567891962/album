0252eb2eb2f7fb9442c0fffaa245cef7fb99c9de
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-20 11:07:13 -0500

    Create object to save data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,10 +3,12 @@ import {Text, View} from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = {albums: []};
+
   componentWillMount() {
     axios
       .get('http://rallycoding.herokuapp.com/api/music_albums')
-      .then((response) => console.log(response));
+      .then((response) => this.setState({albums: response.data}));
   }
 
   render() {
