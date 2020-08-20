66b9dbb146944c7e02ad56f4c068a045001125a0
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-20 13:09:19 -0500

    Create components to divde the app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, AppRegistry, View} from 'react-native';
+import { AppRegistry, View} from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 const App = () => {
